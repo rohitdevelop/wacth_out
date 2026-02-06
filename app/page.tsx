03df99e554a14931c65e994e72a98ba710772e93
Rohit Singh <rohitdev124421@gmail.com>
2026-02-06 18:10:09 +0530

    Fix git email and new contribution

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
- import React from 'react'
- 
- const page = () => {
-   return (
-     <div className='h-screen w-full bg-amber-100'>
-       <div className="bg-red-400">
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="h-screen w-full bg-amber-100">
+      <div className="bg-red-400">
         <h1>hello everony</h1>
-       </div>
-     </div>
-   )
- }
- 
- export default page
- 
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur incidunt harum itaque ipsum libero, debitis reiciendis!
+          Aliquid minus provident reiciendis libero nulla temporibus, explicabo
+          amet eligendi dignissimos eos soluta, ab reprehenderit, eius molestiae
+          unde recusandae incidunt a excepturi! Neque at repudiandae est ullam
+          odit.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
