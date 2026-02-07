f52b0b3fcbe33d777b5da090097acf87d3707033
Rohit Singh <rohitdev124421@gmail.com>
2026-02-07 21:36:20 +0530

    aksjdf

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className="h-screen w-full bg-amber-100">
       <div className="bg-red-400">
-        <h1>hello everony</h1>
+        <h1>hello easdfasverony</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
           consequuntur incidunt harum itaque ipsum libero, debitis reiciendis!
