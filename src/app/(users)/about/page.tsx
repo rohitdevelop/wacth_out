db1cb29e7111cbdd97be5082058cde4de2b88c09
Rohit Singh <rohitdev124421@gmail.com>
2026-02-20 23:37:42 +0530

    tested done

---
+++ src/app/(users)/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen w-full flex justify-center items-center bg-black text-white'>
+        <h2 className='text-5xl'>hello about</h2>
+    </div>
+  )
+}
+
+export default page
