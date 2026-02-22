d8d8007fa009036772ec284f2bd8f39c166f0607
Rohit Singh <rohitdev124421@gmail.com>
2026-02-22 17:41:20 +0530

    font error fix

--- src/app/(users)/about/page.tsx
+++ src/app/(users)/about/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-
+ 
 export default function Page() {
   return (
-    <div className="h-screen w-full flex justify-center items-center bg-black text-white">
-      <h2 className="font-(--font-playfair) text-5xl">hello about</h2>
+    <div className="h-screen w-full flex justify-center items-center gap-6 bg-black text-white">
+      <h2 className="font-mono text-5xl">mono</h2>
+      <br />
+      <h2 className="font-(--font-montserrat) text-5xl"> ROHIT</h2>
+      <h2 className="font-(--font-montserrat) text-5xl"> rohit</h2>
+      <br />
+      <h2 className="font-(--font-playfair) text-5xl">ROHIT</h2>
+      <h2 className="font-(--font-playfair) text-5xl">rohit</h2>
     </div>
   );
 }
--- src/app/(users)/layout.tsx
+++ src/app/(users)/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${montserrat.variable} ${playfair.variable}`}>
-      <body className="font-(--font-montserrat)">
+      <body className="font-(--font-playfair)">
         <Navbar />
         {children}
       </body>
