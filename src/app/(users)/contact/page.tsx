7ab84b7858eb31983df3686148f48f9a756c9a4f
Rohit Singh <rohitdev124421@gmail.com>
2026-03-05 19:33:19 +0530

    same pages created

--- src/app/(users)/contact/page.tsx
+++ src/app/(users)/contact/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
- 
-export default function Page() {
+const page = () => {
   return (
     <div className="h-screen w-full flex justify-center items-center gap-6 bg-black text-white">
- <h1>hello about</h1>
+ <h1>hello contact</h1>
     </div>
-  );
+  )
 }
+
+export default page
---
+++ src/app/(users)/sell/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+       <div className="h-screen w-full flex justify-center items-center gap-6 bg-black text-white">
+ <h1>hello sell</h1>
+    </div>
+  )
+}
+
+export default page
