783e8921467767330636f9ce857e935fceae13ed
Rohit Singh <rohitdev124421@gmail.com>
2026-04-11 21:10:35 +0530

    url error

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const devUrl = process.env.NEXT_PUBLIC_DEVLOPMENT_URL;
 const prodUrl = process.env.NEXT_PUBLIC_PRODUCTION_URL;
 
-const baseURL =
-  process.env.NODE_ENV === "development" ? devUrl : prodUrl;
+const baseURL = "https://wacth-app.onrender.com/api/"
+   
 
 const api = axios.create({
   baseURL,
